1f1798e053f0a148cf4bec940a2c86b8a2df39e0
Daniel <danielnijkamp2004@gmail.com>
2021-09-14 15:10:13 +0200

    Added functionality to main menu buttons and settings menu back button + updated README.md

--- README.md
+++ README.md
@@ -8,5 +8,9 @@ Tower defense project
 [Trello](https://trello.com/b/wgsrcJFx/tower-defense)
 
 
+##Screenshots
+[Main_Menu](https://media.discordapp.net/attachments/862814301873504256/887318277313929236/unknown.png)
+[Settings](https://cdn.discordapp.com/attachments/854772935573897249/887324114497449984/unknown.png)
+
 # License
 [MIT](https://cdn.discordapp.com/attachments/854772935573897249/885946058625151006/Trollface_non-free.png)
--- Tower_Defense/Assets/Scripts/MainMenuScript.cs
+++ Tower_Defense/Assets/Scripts/MainMenuScript.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
 
 public class MainMenuScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject MainMenuCanvas;
+    //public GameObject LoadingCanvas;
+    public GameObject SettingsCanvas;
+    public GameObject SecretCanvas;
 
-    // Update is called once per frame
-    void Update()
+    //Loading screen wip
+
+
+    //public Slider slider;
+    //public TextMeshProUGUI ProgressText;
+
+    //public GameObject MainMenuBackButton;
+    public void PlayGame(int sceneIndex)
     {
-        
+        //StartCoroutine(LoadAsync(sceneIndex));
     }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+   
+    /*IEnumerator LoadAsync(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync("GameScene");
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            slider.value = progress;
+            ProgressText.text = progress * 100f + "%";
+            yield return null;
+        }
+        if (operation.isDone)
+        {
+            LoadingCanvas.SetActive(false);
+
+        }
+    }*/
+
+
+
+
 }
